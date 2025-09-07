edb7780c6fbd3e4ddb71d5967a5114288fe44abc
cors_576 <you@example.com>
2025-09-07 14:47:01 +0300

    New Link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -59,7 +59,7 @@ const Footer = () => {
         <p>
           Проект реализован при поддержке{" "}
           <a
-            href="https://foresat.tilda.ws/ "
+            href="https://fasie.ru/"
             target="_blank"
             rel="noopener noreferrer"
             className="footer-link"
